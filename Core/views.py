f7700ca2160f103387148a3b9ad0cedd06b80040
Nursultan301 <abykeevnursultan14@gmail.com>
2022-03-31 19:51:09 +0600

    Updated models.py

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Announcements
+
+
+def test(request):
+    context = Announcements.objects.all()
+    return render(request, 'Core/index.html', {"obj": context})
--- Obd_center/settings.py
+++ Obd_center/settings.py
@@ -64,10 +64,15 @@ WSGI_APPLICATION = 'Obd_center.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Obd_center',
+        'USER': 'Nursultan',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
