f1d2c79b95587f5d687ea1da86d5ef6b26037ecd
Nursultan301 <abykeevnursultan14@gmail.com>
2023-04-19 22:46:46 +0600

    Accidentally deleting and adding again

---
+++ Obd_center/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('test/', include("Test.urls")),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
+
